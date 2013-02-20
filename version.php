2029e4bf1c07748414b5bb27e5a0e13d2000fb08
Jean FRUITET <jean.fruitet@free.fr>
2013-02-20 11:41:24 +0100

    Caractère '`' retiré de toutes les requêtes SQL car il est problématique pour PostgreSQL
    Signed-off-by: Jean FRUITET <jean.fruitet@free.fr>

--- version.php
+++ version.php
@@ -14,7 +14,7 @@ $module=new StdClass();
 
 $module->requires = 2011120500;  // Requires this Moodle version  2.0
 $module->version  = 2013021700;  // The current module version (Date: YYYYMMDDXX)
-$module->release  = 'Referentiel v 8.04 for Moodle 2.4 - 2013-02-17';    // User-friendly date of release
+$module->release  = 'Referentiel v 8.04 for Moodle 2.4 - 2013-02-20';    // User-friendly date of release
 $module->cron     = 60; //  Period for cron to check this module (secs)
 
 ?>
